3b575045a5d08cdc85269c23a384e31174f6d73d
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2019-09-15 23:20:51 +0530

    Update navigation

--- src/components/AppHeader.svelte
+++ src/components/AppHeader.svelte
@@ -5,10 +5,16 @@
     <div class="xl:flex -mx-6">
         <div class="px-6 text-left md:text-center xl:text-left max-w-2xl md:max-w-3xl mx-auto">
             <h1 class="text-3xl sm:text-4xl md:text-5xl xl:text-4xl font-light leading-tight"><strong>Svelte</strong> <span class="text-teal-500 font-normal">Pizzeria</span></h1>
+              <nav>
+                 <ul class="flex">
+                    <li class="flex-1 mr-2 text-center">
+                        <NavLink to="/" class="text-center block border border-blue-500 rounded py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white">Home</NavLink>
+                    </li>
+                    <li class="flex-1 mr-2 text-center">
+                        <NavLink to="about" class="text-center block border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-2 px-4">About</NavLink>
+                    </li>
+                </ul>
+              </nav>
         </div>
-          <nav>
-    <NavLink to="/">Home</NavLink>
-    <NavLink to="about">About</NavLink>
-  </nav>
     </div>
 </div>
