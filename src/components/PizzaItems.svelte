0926a283b0f37ccd60241cd08a6bbc093e9d05fe
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2019-09-15 22:08:48 +0530

    Re-factor PizzaItems

--- src/components/PizzaItems.svelte
+++ src/components/PizzaItems.svelte
@@ -1,6 +1,4 @@
 <script>
-    import Icon  from 'svelte-awesome/components/Icon.svelte';
-    // import { refresh } from 'svelte-awesome/icons';
     import PizzaItem from './PizzaItem.svelte';
     export let items = [];
 
@@ -9,6 +7,7 @@
     let nextId = 0;
 
     function addToCart(event) {
+        console.log(event);
         if (event.key === 'Enter') {
             todos = [...cart, {
                 id: nextId,
@@ -20,26 +19,25 @@
 </script>
 
 <style>
-    .container {
-        max-width: 800px;
-        margin: 10px auto;
+    .pizza-list {
+        width: calc(100vw - 200px);
+        height: auto;
+        min-height: calc(100vh);
+        max-width: 1024px;
+        margin-left: 150px;
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        grid-column-gap: 30px;
+        grid-row-gap: 40px;
     }
-
-    /* .active {
-        background: lightseagreen;
-    } */
 </style>
 
 
-<div class="container">
-    <div class="pt-12">
-        {#each items as pizza, index (pizza.id)}
-            <div class="pizza-item">
-                {pizza.name}
-                <!-- <TodoItem {...todo} on:deleteTodo={handleDeleteTodo} on:toggleComplete={handleToggleComplete} /> -->
-            </div>
-        {/each}
-    </div>
-    <!-- <Icon data={refresh} scale="3"></Icon> -->
+<div class="pt-12 pizza-list">
+    {#each items as pizza, index (pizza.id)}
+        <div class="pizza-item">
+            <PizzaItem {pizza} on:addPizza={addToCart}/>
+        </div>
+    {/each}
 </div>
 
