c3c579a2450708ea50076c464c9a4623b663ea28
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2019-09-16 21:17:49 +0530

    Update menu items

--- src/components/AppHeader.svelte
+++ src/components/AppHeader.svelte
@@ -2,12 +2,20 @@
     import { getContext } from 'svelte';
     import Icon  from 'svelte-awesome/components/Icon.svelte';
     import { shoppingBag } from 'svelte-awesome/icons';
+    // pizzaSlice, carrot, cocktail
     import NavLink from "./NavLink.svelte";
 
     const cart = getContext('cart')
     $: cartItemCount = $cart.length;
 
 </script>
+
+<style>
+    :global(a.active) {
+        color: green;
+        font-weight: bold;
+    }
+</style>
 <div class="w-full max-w-screen-xl relative mx-auto px-6 pt-12 pb-12 lg:border-b-2 lg:border-gray-200 sticky">
     <div class="xl:flex -mx-6">
         <div class="px-6 text-left md:text-center xl:text-left max-w-2xl md:max-w-3xl mx-auto">
@@ -16,17 +24,17 @@
                  <ul class="flex">
                     <li class="flex-1 mr-2 text-center">
                         <NavLink to="/" class="text-center block border border-blue-500 rounded py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white">
-                            Pizzas
+                            <!-- <Icon data={pizzaSlice} scale={2}></Icon>--> Pizzas
                         </NavLink>
                     </li>
                     <li class="flex-1 mr-2 text-center">
                         <NavLink to="drinks" class="text-center block border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-2 px-4">
-                            Drinks
+                            <!-- <Icon data={cocktail} scale={2}></Icon> --> Drinks
                         </NavLink>
                     </li>
                     <li class="flex-1 mr-2 text-center">
                         <NavLink to="salads" class="text-center block border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-2 px-4">
-                            Salads
+                            <!-- <Icon data={carrot} scale={2}></Icon>Salads --> Salads
                         </NavLink>
                     </li>
                 </ul>
