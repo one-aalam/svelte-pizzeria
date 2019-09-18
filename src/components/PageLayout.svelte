5afabc672b0cf1a4902de3af4c1de679c961eed6
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2019-09-18 20:57:58 +0530

    Fix scroll issue

--- src/components/PageLayout.svelte
+++ src/components/PageLayout.svelte
@@ -1,3 +1,11 @@
+<script>
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        window.scrollTo(0, 0);
+    })
+</script>
+
 <div class="bg-gray-100 inset-x-0 top-0 border-b-2 border-gray-200 lg:border-b-0 lg:static flex items-center pt-12 pb-12">
     <slot></slot>
 </div>
