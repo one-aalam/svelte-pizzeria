370c47fcb3188463d0f068285990d6fbae4ff2cb
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2019-09-20 00:17:15 +0530

    Add details in footer

--- src/components/AppFooter.svelte
+++ src/components/AppFooter.svelte
@@ -5,5 +5,5 @@
             </h1>
         </div>
     </div>
-    <div>This project is open source on github, hosted with Netlify, and made with love by Aftab Alam(aalam.in), @aftabbuddy (twitter) & @one-aalam (github)</div>
+    <div>This project is Open Source on <a href="https://github.com/one-aalam/svelte-pizzeria">github</a>, hosted with <a href="https://optimistic-euclid-892bac.netlify.com">Netlify</a>, and made with love by Aftab Alam(<a href="http://aalam.in/">aalam.in</a>), <a href="https://twitter.com/aftabbuddy">@aftabbuddy</a> (twitter) & <a href="https://twitter.com/aftabbuddy">https://github.com/one-aalam</a> (github)</div>
 </div>
