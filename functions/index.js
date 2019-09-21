3a22f6a112a29929281542bc3720565567db0828
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2019-09-21 15:56:16 +0530

    Update charge function

--- functions/index.js
+++ functions/index.js
@@ -7,18 +7,17 @@ const headers = {
   'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
   'Access-Control-Allow-Methods': '*'
 }
-// 'Content-Type': 'application/json',
 // 'Access-Control-Max-Age': '2592000',
 // 'Access-Control-Allow-Credentials': 'true'
 
 exports.handler = async (event, context, callback) => {
   // In the case of a CORS preflight check, just return early.
-  if ( event.httpMethod === "OPTIONS" ) {
+  if (event.httpMethod === "OPTIONS") {
     callback(
         null,
         {
             statusCode: 200,
-            headers: headers,
+            headers,
             body: JSON.stringify( "OK" )
         }
     );
@@ -33,13 +32,9 @@ exports.handler = async (event, context, callback) => {
       })
     };
   }
-  const body = {...event.body }
 
-  console.log('event body', event.body);
-  const data = querystring.parse(body)
+  const data = querystring.parse(event.body)
   if (!data.stripeToken || !data.stripeAmt || !data.stripeIdempotency) {
-    console.error("Required information is missing.")
-
     return {
       statusCode: 400,
       headers,
@@ -49,75 +44,75 @@ exports.handler = async (event, context, callback) => {
     };
   }
 
-  let charge;
+  // let charge;
 
-  try {
-    const customer = stripe.customers.create({ email: data.stripeEmail, source: data.stripeToken })
-    charge = await stripe.charges.create(
-      {
-        amount: data.stripeAmt,
-        currency: 'usd',
-        description: 'Example charge',
-        source: data.stripeToken,
-        receipt_email: data.stripeEmail,
-        customer: customer.id,
-      },
-      {
-        idempotency_key: data.stripeIdempotency
-      }
-    );
-  } catch (e) {
-    let message = e.message;
-  
-    console.error(message);
-
-    return {
-      statusCode: 424,
-      headers,
-      body: JSON.stringify({
-        status: "failed",
-        message
-      })
-    };
-  }
-
-  const status = (charge === null || charge.status !== "succeeded") ? "failed" : charge.status;
-  return {
-    statusCode,
-    headers,
-    body: JSON.stringify({
-      status,
-      message: "Charge successfully created!"
-    })
-  };
-
-  // stripe.customers
-  //   .create({
-  //     email: data.stripeEmail,
-  //     source: data.stripeToken
-  //   })
-  //   .then(customer => stripe.charges.create({
+  // try {
+  //   const customer = stripe.customers.create({ email: data.stripeEmail, source: data.stripeToken })
+  //   charge = await stripe.charges.create(
+  //     {
   //       amount: data.stripeAmt,
   //       currency: 'usd',
   //       description: 'Example charge',
   //       source: data.stripeToken,
   //       receipt_email: data.stripeEmail,
   //       customer: customer.id,
-  //     },{
+  //     },
+  //     {
   //       idempotency_key: data.stripeIdempotency
-  //   }))
-  //   .then(charge => callback(null, {
-  //     statusCode: 200,
-  //     body: charge
-  //   }))
-  //   .catch(err => {
-  //       console.log("Error:", err);
-  //       callback({
-  //         statusCode: 500,
-  //         headers,
-  //         body: JSON.stringify({
-  //           status: "Purchase failed"
-  //         })
-  //       })
-  //   });
+  //     }
+  //   );
+  // } catch (e) {
+  //   let message = e.message;
+  
+  //   console.error(message);
+
+  //   return {
+  //     statusCode: 424,
+  //     headers,
+  //     body: JSON.stringify({
+  //       status: "failed",
+  //       message
+  //     })
+  //   };
+  // }
+
+  // const status = (charge === null || charge.status !== "succeeded") ? "failed" : charge.status;
+  // return {
+  //   statusCode,
+  //   headers,
+  //   body: JSON.stringify({
+  //     status,
+  //     message: "Charge successfully created!"
+  //   })
+  // };
+
+  stripe.customers
+    .create({
+      email: data.stripeEmail,
+      source: data.stripeToken
+    })
+    .then(customer => stripe.charges.create({
+        amount: data.stripeAmt,
+        currency: 'usd',
+        description: 'Example charge',
+        source: data.stripeToken,
+        receipt_email: data.stripeEmail,
+        customer: customer.id,
+      },{
+        idempotency_key: data.stripeIdempotency
+    }))
+    .then(charge => callback(null, {
+      statusCode: 200,
+      body: charge
+    }))
+    .catch(err => {
+        console.log("Error:", err);
+        callback({
+          statusCode: 500,
+          headers,
+          body: JSON.stringify({
+            status: "Purchase failed"
+          })
+        })
+    });
 }
